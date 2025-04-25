4fd3032273ca62cca55fcdb482ccdefaf5aa7169
tali-bd <taliblando@gmail.com>
2025-04-25 11:18:49 -0700

    Lab finished
    Had issue with the "Forgot username?" button, now fixed. Should perform the following functions:
    - Type in username
      - Subsequent logging in should take to next page and display typed username as title
    - Press "Forgot username"?, should take to next page where title is "Forgot Username"
    - Press "Forgot password?", should take to next page where title is "Forgot Password?"

--- LabLogin/ViewController.swift
+++ LabLogin/ViewController.swift
@@ -9,22 +9,44 @@ import UIKit
 
 class ViewController: UIViewController {
     
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
     
     override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        segue.destination.navigationItem.title = Username.text
+        
+        guard let sender = sender as? UIButton else { return }
+
+        if sender == forgotPasswordButton {
+            segue.destination.navigationItem.title = "Forgot Password"
+        }
+        else if sender == forgotUsernameButton {
+            segue.destination.navigationItem.title = "Forgot Username"
+        }
+        else {
+            segue.destination.navigationItem.title = Username.text
+        }
     }
     
     @IBOutlet var Username: UITextField!
     
-
     @IBOutlet var forgotUsernameButton: UIButton!
     
-    
     @IBOutlet var forgotPasswordButton: UIButton!
+    
+    
+    @IBAction func forgotUsernameAction(_ sender: Any) {
+        performSegue(withIdentifier: "moveToLandingScreen", sender: sender)
+    }
+    
+    @IBAction func forgotPasswordAction(_ sender: Any) {
+        
+        performSegue(withIdentifier: "moveToLandingScreen", sender: sender)
+        
+    }
+    
     
 }
 
